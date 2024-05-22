d11aa521374f70ecddb838822764ddb6a20cbc25
Rogerio-17 <rogeriojmf10@gmail.com>
2024-05-21 21:18:07 -0300

    feat: created the project, configured and finalized the home screen

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { FormEvent, useState } from 'react'
+import { FiSearch } from 'react-icons/fi'
+
+export function Input() {
+    const [input, setInput] = useState('')
+    const router = useRouter()
+
+    function handleSearch(event: FormEvent) {
+        event.preventDefault()
+
+        if (input === '') return
+
+        router.push(`/game/search/${input}`)
+    }
+
+    return (
+        <form
+            onSubmit={handleSearch}
+            className="w-full bg-slate-200 my-5 flex gap-2 items-center justify-between rounded-lg p-2"
+        >
+            <input
+                type="text"
+                placeholder="Procurando algum jogo?"
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
+                className="bg-transparent outline-none w-11/12"
+            ></input>
+            <button type="submit">
+                <FiSearch size={16} color="#ea580c" />
+            </button>
+        </form>
+    )
+}
